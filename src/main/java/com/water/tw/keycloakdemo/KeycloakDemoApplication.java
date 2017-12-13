67cdf81bab70bb791683fba906bcc27606e442bd
Waterstrong <sqlin@thoughtworks.com>
2017-12-13 13:16:12 +0800

    init the keycloak demo

---
+++ src/main/java/com/water/tw/keycloakdemo/KeycloakDemoApplication.java
@@ -0,0 +1,12 @@
+package com.water.tw.keycloakdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakDemoApplication.class, args);
+	}
+}
